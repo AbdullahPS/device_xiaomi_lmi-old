512e8d82e391cceb9449851b525c62bd70ed82d5
Abdullah <abdullahtabbal@github.com>
2020-12-20 12:00:21 +0000

    remove pixel props and fp

--- aosp_lmi.mk
+++ aosp_lmi.mk
@@ -9,12 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-# Inherit some common PixelExperience stuff.
+
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
+#TARGET_GAPPS_ARCH := arm64
+#TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/telephony.mk)
+ 
+
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_lmi
@@ -23,15 +26,5 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := POCO F2 PRO
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+#PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DISC="lmi-user 11 RKQ1.200826.002 V12.2.1.0.RJKMIXM release-keys" \
-    PRODUCT_DEVICE=lmi \
-    PRODUCT_NAME=lmi
-
-BUILD_FINGERPRINT := POCO/lmi_global/lmi:11/RKQ1.200826.002/V12.2.1.0.RJKMIXM:user/release-keys
-
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
