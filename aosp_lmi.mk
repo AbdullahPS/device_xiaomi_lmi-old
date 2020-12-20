1b283a5f7db789661d3b2661c9cbfdda0956adf9
Abdullah <abdullahtabbal@github.com>
2020-12-20 22:07:31 +0000

    inherit from aosp root common file

--- aosp_lmi.mk
+++ aosp_lmi.mk
@@ -3,21 +3,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+#Inherit from aex common
+$(call inherit-product, vendor/aosp/common.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
-
-
 TARGET_BOOT_ANIMATION_RES := 1080
-#TARGET_GAPPS_ARCH := arm64
-#TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-$(call inherit-product, vendor/aosp/config/common.mk)
-$(call inherit-product, vendor/aosp/config/telephony.mk)
- 
-
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_lmi
@@ -26,5 +20,5 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := POCO F2 PRO
 
-#PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
